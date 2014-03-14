e4426e74ed693a8c8176e9958639899bb189ee86
David Adalberth Andersen <david@stupid-studio.com>
2014-03-14 16:42:27 +0100

    Updated grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         watch: {
             files: ['*.html','*.js','**/*.scss', '*.rb'],
-            tasks: ['compass:dev', 'uglify:minified', 'concat'],
+            tasks: ['compass:dev',
             options: {
                 livereload:true
             }
